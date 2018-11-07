e3512ea11c4d2896b9d66859fb96275ed4c62404
Aldis Ameriks <aldis.ameriks@gmail.com>
2018-11-08 00:47:17 +0200

    POC for making requests to internalgo

--- main.go
+++ main.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
+
+	var netClient = &http.Client{
+		Timeout: time.Second * 3,
+	}
+
+	resp, err := netClient.Get("http://internalgo.default.svc.cluster.local:8081")
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+	var parsedBody = string(body)
+	fmt.Println(parsedBody)
 	fmt.Println("Request received")
-	fmt.Fprintf(w, "Welcome to Kubernetes!")
+	fmt.Fprintf(w, "Welcome to Kubernetes! %s", parsedBody)
 }
 
 func main() {
